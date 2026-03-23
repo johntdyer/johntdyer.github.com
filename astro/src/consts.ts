9e88e2c7b613d84687b59d67aef8067a1afbce4c
John Dyer <johntdyer@gmail.com>
2026-03-23 15:13:35 -0400

    init new site

--- astro/src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'johntdyer.com';
-export const SITE_DESCRIPTION = 'John Dyer\'s blog.';
-export const SITE_AUTHOR = 'John Dyer';
